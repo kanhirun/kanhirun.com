73ad9e1f3fcd946d4f402c48f8cb3ce835e69bda
Kel Kanhirun <kel.kanhirun@gmail.com>
2016-04-22 09:57:42 -0500

    feat(resume): displays my resume to the world

--- app/controllers/resume_controller.rb
+++ app/controllers/resume_controller.rb
@@ -1,5 +1,6 @@
 class ResumeController < ApplicationController
   def call
-    render nothing: true
+    pdf = 'app/assets/pdfs/2016-04-21-kkanhirun-resume.pdf'
+    send_file(pdf, filename: 'kel-kanhirun-resume.pdf', type: 'application/pdf', disposition: 'inline')
   end
 end
