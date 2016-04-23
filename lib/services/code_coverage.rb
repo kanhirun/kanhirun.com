b77574b65d2712ba696d062adc6b605196d42e9c
Kel Kanhirun <kel.kanhirun@gmail.com>
2016-04-23 07:47:47 -0500

    Filters service wrappers from code coverage

--- lib/services/code_coverage.rb
+++ lib/services/code_coverage.rb
@@ -3,7 +3,11 @@ require 'simplecov'
 class CodeCoverage
   def self.start!
     SimpleCov.start :rails do
+      # Exit with non-zero if coverage drops below this value:
       minimum_coverage 100
+
+      # Ignore coverage for service wrappers since they should be fairly thin
+      add_filter '/lib/services'
     end
   end
 end
