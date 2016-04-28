6eaed0d72ed20ddccd9ae158ef0180bf6430cd2f
Kel Kanhirun <kel.kanhirun@gmail.com>
2016-04-28 17:09:48 -0500

    Extracts variable into class const

--- app/controllers/my_resume/get_latest_resume.rb
+++ app/controllers/my_resume/get_latest_resume.rb
@@ -1,12 +1,14 @@
 module MyResume
   class GetLatestResume
+    DATE_PATTERN = /\d+-\d+-\d+/
+
     def initialize(pdf_files)
       @pdf_files = pdf_files
     end
 
     def call
       latest_resume = pdf_files.sort_by do |pdf|
-        date_string = pdf.match(/\d+-\d+-\d+/).to_s
+        date_string = pdf.match(DATE_PATTERN).to_s
         Date.parse(date_string)
       end.last
 
